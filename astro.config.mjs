28672caccfb6ec3ea1575dfc0ddd52868abc66f9
Florian Schulz <florian.schulz404@gmail.com>
2026-01-02 15:57:01 +0100

    🎉 start project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: "aciiverse handbook WIP",
+			social: [{ icon: "github", label: "GitHub", href: "https://github.com/aciiverse/handbook" }],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: "Docs",
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: "Example Guide", slug: "guides/example" },
 					],
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: "Reference",
+					autogenerate: { directory: "reference" },
 				},
 			],
 		}),
