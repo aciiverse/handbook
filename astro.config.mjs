c348b3dc01eb409ff3e388e68f9ae475f30c21d0
Florian Schulz <florian.schulz404@gmail.com>
2026-01-03 13:22:31 +0100

    🔧 changed astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://aciiverse.github.io",
+	base: "/handbook/",
 	integrations: [
 		starlight({
 			title: "handbook",
